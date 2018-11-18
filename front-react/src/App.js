91dc659d7fab0bda675f233c7fce667900edb163
TsuyoshiNumano <numano.tsuyoshi@lancers.co.jp>
2018-11-18 16:01:19 +0900

    feat: list view

--- front-react/src/App.js
+++ front-react/src/App.js
@@ -2,8 +2,35 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      emojiList: [
+        {
+          url:
+            'https://emoji.slack-edge.com/T02HHLFPR/lgtm/393f650bcf7d2b0a.png',
+          likeCount: 0
+        }
+      ]
+    };
+  }
+
   render() {
-    return <div>emojishare</div>;
+    return (
+      <div>
+        <div>emojishare</div>
+        <div>
+          {this.state.emojiList.map((emoji, i) => (
+            <img
+              alt={emoji.url}
+              key={'emoji' + i}
+              src={emoji.url}
+              width="20px"
+            />
+          ))}
+        </div>
+      </div>
+    );
   }
 }
 
