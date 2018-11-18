5330e4334d5ff9e459789485c4555d76910addb1
TsuyoshiNumano <numano.tsuyoshi@lancers.co.jp>
2018-11-18 16:09:03 +0900

    feat: input field and button

--- front-react/src/App.js
+++ front-react/src/App.js
@@ -11,14 +11,36 @@ class App extends Component {
             'https://emoji.slack-edge.com/T02HHLFPR/lgtm/393f650bcf7d2b0a.png',
           likeCount: 0
         }
-      ]
+      ],
+      inputValue: ''
     };
   }
+
+  hadleChange = e => {
+    this.setState({ inputValue: e.target.value });
+  };
+
+  handleSubmit = e => {
+    this.setState({
+      emojiList: [
+        ...this.state.emojiList,
+        { url: this.state.inputValue, likeCount: 0 }
+      ]
+    });
+    this.setState({ inputValue: '' });
+  };
 
   render() {
     return (
       <div>
         <div>emojishare</div>
+        <input
+          type="text"
+          placeholder="Please add your Emoji's url"
+          value={this.state.inputValue}
+          onChange={this.hadleChange}
+        />
+        <button onClick={this.handleSubmit}>add</button>
         <div>
           {this.state.emojiList.map((emoji, i) => (
             <img
